472e69bdf44759ea08fdb9f16891c4eb976c1e3e
Believe.You <846829525@qq.com>
2022-12-09 13:56:06 +0800

    fix：修复无法签到

--- demos/xunacg/xunacg.go
+++ demos/xunacg/xunacg.go
@@ -5,6 +5,7 @@ package xunacg
 import (
 	"encoding/json"
 	"fmt"
+	"strconv"
 	"time"
 
 	"github.com/gocolly/colly"
@@ -16,18 +17,22 @@ type Result struct {
 }
 
 func SignByUser(currentUser XunAcgUser, c *colly.Collector, max int) {
+	url := "https://www.xunacg.xyz/my-free.htm"
+	requestData := map[string]string{
+		"uid": strconv.Itoa(currentUser.Uid),
+	}
 
 	c.OnRequest(func(r *colly.Request) {
 		// fmt.Println("Cookie", currentUser.Cookie)
-		r.Headers.Add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.62")
+		r.Headers.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.62")
 		// r.Headers.Set(":method", "POST")
 		// r.Headers.Set(":path", "/my-free.htm")
 		// r.Headers.Set(":scheme", "https")
-		r.Headers.Set("origin", "https://www.xunacg.xyz")
-		r.Headers.Set("content-type", "application/x-www-form-urlencoded; charset=UTF-8")
+		r.Headers.Set("Origin", "https://www.xunacg.xyz")
+		r.Headers.Set("Content-Type", "application/x-www-form-urlencoded")
 		// r.Headers.Add(":authority", "www.xunacg.xyz")
-		r.Headers.Set("x-requested-with", "XMLHttpRequest")
-		r.Headers.Set("cookie", currentUser.Cookie)
+		r.Headers.Set("X-Requested-With", "XMLHttpRequest")
+		r.Headers.Set("Cookie", currentUser.Cookie)
 	})
 	c.OnError(func(r *colly.Response, err error) {
 		fmt.Println("请求失败：" + err.Error())
@@ -36,20 +41,23 @@ func SignByUser(currentUser XunAcgUser, c *colly.Collector, max int) {
 	c.OnResponse(func(response *colly.Response) {
 
 		var result Result
-		err := json.Unmarshal(response.Body, &result)
+		err := json.Unmarshal(response.Save(), &result)
 		if err != nil {
 			fmt.Println("签到失败:" + string(response.Body))
 		} else if result.Code == "0" {
+			currentUser.Count += 1
+
 			fmt.Printf("已签到: %d 次,%s \n ", currentUser.Count, string(response.Body))
+
 			if max > 0 && currentUser.Count >= max {
 				fmt.Printf("%s 签到已完，共 %d 次", currentUser.Name, currentUser.Count)
 				return
 			}
-			currentUser.Count += 1
+
 			// 等待下一次
 			time.Sleep(40 * time.Second)
 
-			c.PostRaw("https://www.xunacg.xyz/my-free.htm", []byte("uid="+(string)(currentUser.Uid)))
+			c.Post(url, requestData)
 		} else if result.Code == "-1" {
 			//fmt.Printf("%s 签到已完，共 %d 次", currentUser.Name, currentUser.Count)
 			fmt.Println(result.Message)
@@ -58,7 +66,8 @@ func SignByUser(currentUser XunAcgUser, c *colly.Collector, max int) {
 	})
 	fmt.Printf("开始签到:%s(%d) \n", currentUser.Name, currentUser.Uid)
 
-	c.PostRaw("https://www.xunacg.xyz/my-free.htm", []byte("uid="+(string)(currentUser.Uid)))
+	// c.PostRaw(url, []byte("uid="+))
+	c.Post(url, requestData)
 
 }
 
